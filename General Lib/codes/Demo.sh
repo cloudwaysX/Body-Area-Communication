48b63e9676da79d88e7ed5ab1dc3f3117a9aae0a
U-LANGROUP\cheny <cheny@MSEI-Q7488.biotronik.com>
2017-07-05 16:27:07 -0700

    hier block update, FSK finished

--- General Lib/codes/Demo.sh
+++
@@ -1,6 +0,0 @@
-python Signal_Generator.py
-python MSK_FSK_mod.py
-cp ../Files/GLFSR_#_16384_4000K_270K_4K_0_RX ../Files/GLFSR_#_16384_4000K_270K_4K_0_TX
-python Freq_offset_helper.py
-python MSK_FSK_demod.py
-python Error_Check.py
